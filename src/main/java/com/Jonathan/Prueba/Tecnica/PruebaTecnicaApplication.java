e47fe1e4c570821d7da8902f1a09360b7f14e286
Jonathan Urrego <jadougdev@gmail.com>
2025-05-07 01:28:21 -0500

    config

--- src/main/java/com/Jonathan/Prueba/Tecnica/PruebaTecnicaApplication.java
+++ src/main/java/com/Jonathan/Prueba/Tecnica/PruebaTecnicaApplication.java
@@ -2,12 +2,18 @@ package com.Jonathan.Prueba.Tecnica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PruebaTecnicaApplication {
+public class PruebaTecnicaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaTecnicaApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PruebaTecnicaApplication.class);
+	}
 }
